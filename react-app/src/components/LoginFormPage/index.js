ac78c5754f6b71db28a60501e9a5ee7aaea951ca
Your Name <Your Email>
2023-05-29 21:50:04 -0500

    spelling

--- react-app/src/components/LoginFormPage/index.js
+++ react-app/src/components/LoginFormPage/index.js
@@ -35,7 +35,7 @@ function LoginFormPage() {
     <div id="loginPage">
       <div id="loginText">
         <h1 id="logoHere">FacePamphlet</h1>
-        <h3 id="infoText">Connect with friends and the world around tou with FacePamphlet.</h3>
+        <h3 id="infoText">Connect with friends and the world around you with FacePamphlet.</h3>
       </div>
       <div >
         <form onSubmit={handleSubmit} id="loginForm">
